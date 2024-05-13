4e5ad8e775a8a3f92312deafae261cb0e43ad5e7
Vahe <vegastar000@mail.ru>
2024-05-13 16:37:26 +0400

    registration using confirmation code DONE

--- pong/authentication/static/confirm.js
+++ pong/authentication/static/confirm.js
@@ -1,53 +1,62 @@
-var confirmation_code;
-
-function moveFocus(current, next) {
+function moveFocus(current, next)
+{
     var length = document.getElementById(current).value.length;
     var maxLength = document.getElementById(current).getAttribute("maxlength");
 
-    if (length >= maxLength && next) {
+    if (length >= maxLength && next)
+    {
         document.getElementById(next).focus();
     }
 }
 
 function confirmCode() {
     var code = "";
-    for (var i = 1; i <= 5; i++) {
+    for (var i = 1; i <= 5; i++)
+    {
         code += document.getElementById("digit" + i).value;
     }
+    var confirmation_code = localStorage.getItem('confirmation_code');
 
-    console.log("code =", code);
-    console.log("confirmation_code =", confirmation_code);
-
-    if (code === window.confirmation_code) { // Replace "12345" with the actual confirmation code
+    if (code === confirmation_code)
+    {
         document.getElementById("message").innerText = "Confirmation successful!";
         document.getElementById("message").classList.add("success");
         document.getElementById("message").style.display = "block";
         document.querySelector(".loading").style.display = "block";
-        setTimeout(function() {
+        setTimeout(function()
+        {
             window.location.href = "/home/"; // Redirect to another page after 2 seconds
         }, 2000);
-    } else {
+    }
+    else
+    {
         document.getElementById("message").innerText = "Invalid confirmation code. Please try again.";
         document.getElementById("message").classList.add("error");
         document.getElementById("message").style.display = "block";
         
-        for (var i = 1; i <= 5; i++) {
+        for (var i = 1; i <= 5; i++)
+        {
             document.getElementById("digit" + i).value = "";
         }
         document.getElementById("digit1").focus();
     }
 }
 
-document.querySelectorAll('.confirmation-code input').forEach(function(input) {
-    input.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
+document.querySelectorAll('.confirmation-code input').forEach(function(input)
+{
+    input.addEventListener('keydown', function(event)
+    {
+        if (event.key === 'Enter')
+        {
             confirmCode();
         }
     });
 });
 
-document.querySelector('button').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
+document.querySelector('button').addEventListener('keydown', function(event)
+{
+    if (event.key === 'Enter')
+    {
         confirmCode();
     }
 });
--- pong/authentication/static/registration.js
+++ pong/authentication/static/registration.js
@@ -14,55 +14,39 @@ function togglePasswordVisibility(inputId) {
 }
 
 document.getElementById("forSubmit").addEventListener("submit", function(event) {
-    if (!validateForm()) {
+    if (!validateForm())
+    {
         event.preventDefault(); // Prevent the form from submitting
         alert("Please, fill in all fields!"); // Show an alert message
     }
-    else if (!validatePasswords() && validateForm()) {
+    else if (!validatePasswords() && validateForm())
+    {
         event.preventDefault(); // Prevent the form from submitting
         alert("Passwords do not match!"); // Show an alert message
     }
-    else {
+    else
+    {
         event.preventDefault();
         var randomNumber = Math.floor(Math.random() * 90000) + 10000;
         var confirmation_code = randomNumber.toString();
-        console.log("confirmation_code =", confirmation_code);
+        
+        localStorage.setItem('confirmation_code', confirmation_code)
+       
         emailjs.init("19x4j0o_9StkaTZq-");
         var templateParams = {
-            to_name: "Aram",
+            to_name: document.getElementById("name").value,
             code: confirmation_code,
             email: document.getElementById("email").value
         };
         emailjs.send("service_en491cd", "template_jhsyjbb", templateParams)
-        .then(function(response) {
+        .then(function(response)
+        {
             console.log("Email sent successfully:", response);
-        }, function(error) {
+            window.location.href = "/confirm/";
+        }, function(error)
+        {
             console.error("Failed to send email:", error);
         });
-        // var email = document.getElementById("email").value;
-        // fetch("http://localhost:8000/confirm/", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     body: JSON.stringify({
-        //         email: email,
-        //     })
-        // })
-        // .then(response => {
-        //     if (response.ok) {
-        //         // If the request was successful, redirect to the confirmation page
-        //         window.location.href = "/confirm/";
-        //     } else {
-        //         // If there was an error, display an error message
-        //         alert("Failed to send confirmation email. Please try again.");
-        //     }
-        // })
-        // .catch(error => {
-        //     console.error("Error:", error);
-        //     alert("An error occurred. Please try again later.");
-        // });
-        window.location.href = "/confirm/"; // Redirect to page2.html
     }
 });
 
